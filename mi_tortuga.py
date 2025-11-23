d531e821f76cc05e62dd34b4f3ed50636a90d022
valentinaquevedo <nicole.quevedo@est.iudigital.edu.co>
2025-11-23 17:43:13 -0500

    Refine comments in mi_tortuga.py
    Updated comments and descriptions for clarity.

--- mi_tortuga.py
+++ mi_tortuga.py
@@ -1,9 +1,9 @@
-## Tarea 2 - Movimiento de una tortuga 
+## Tarea 2 - Ejercicios de la unidad 1 movimiento de una tortuga 
 
 #Ejercicio 1 y 2
 En este paso hicimos y creamos la tortuga para que su dirección fuera hacia abajo con el siguiente codigo:
-import turtle
 
+import turtle
 t = turtle.Turtle()   # Crea una tortuga
 t.right(90) 
 t.forward(100)
@@ -11,7 +11,8 @@ turtle.done()
 
 # Ejercicio 3 
 Con el siguiente codigo podemos hacer que la tortuga gire 90 grados hacia la derecha:
-  import turtle
+ 
+import turtle
 t = turtle.Turtle()
 t.forward(90)
 t.right(90)          # Gira 90 grados a la derecha
@@ -19,7 +20,8 @@ t.forward(60)
 turtle.done()
 
 # Ejercicio 4 y 5
-en este paso juntamos los anteriores codigos y le ponemos la funcion left que lo hace lo que ya esta hecho pero en forma de cadena para que la tortuga baje en forma de escalerita con los siguientes codigos:
+en este paso juntamos los anteriores codigos y le ponemos la funcion left (realiza lo que ya esta hecho pero en forma de cadena) para que la tortuga baje en forma de escalerita con los siguientes codigos:
+
 import turtle
 t = turtle.Turtle()
 t.forward(90)
