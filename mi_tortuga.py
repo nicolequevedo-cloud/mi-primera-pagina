3307af0f67b461df51beb20d525b9801e5ba01be
valentinaquevedo <nicole.quevedo@est.iudigital.edu.co>
2025-11-23 17:16:20 -0500

    Implement turtle movement exercises in mi_tortuga.py
    Added exercises demonstrating turtle movements and rotations.

--- mi_tortuga.py
+++ mi_tortuga.py
@@ -1,9 +1,43 @@
 ## Tarea 2 - Movimiento de una tortuga 
 
+#Ejercicio 1 y 2
+En este paso hicimos y creamos la tortuga para que su dirección fuera hacia abajo con el siguiente codigo:
 import turtle
 
 t = turtle.Turtle()   # Crea una tortuga
-t.right(90)
+t.right(90) 
 t.forward(100)
 turtle.done()  
+
+# Ejercicio 3 
+Con el siguiente codigo podemos hacer que la tortuga gire 90 grados hacia la derecha:
+  import turtle
+t = turtle.Turtle()
+t.forward(90)
+t.right(90)          # Gira 90 grados a la derecha
+t.forward(60)
+turtle.done()
+
+# Ejercicio 4 y 5
+en este paso juntamos los anteriores codigos y le ponemos la funcion left que lo hace lo que ya esta hecho pero en forma de cadena para que la tortuga baje en forma de escalerita con los siguientes codigos:
+import turtle
+t = turtle.Turtle()
+t.forward(90)
+t.right(90)          # Gira 90 grados a la derecha
+t.forward(60)
+t.left(90)
+t.forward(90)
+t.right(90)          # Gira 90 grados a la derecha
+t.forward(60)
+t.left(90)
+t.forward(90)
+t.right(90)          # Gira 90 grados a la derecha
+t.forward(60)
+turtle.done()
+
+### 
+
+
+
+
 
